d50cb3d7f32695abbabb1110047dae14338e6ca3
INFINITE4LIFEYT <arnaykumar@outlook.com>
2023-12-23 10:37:00 -0600

    Update ddn.js

--- ddn.js
+++ ddn.js
@@ -1,25 +1,7 @@
-// Create the navigation bar
-const navBar = document.createElement('nav');
-navBar.classList.add('navbar');
+document.querySelector('.dropdown').addEventListener('mouseover', function () {
+  this.querySelector('.dropdown-content').classList.add('expanded');
+});
 
-// Create the links
-const homeLink = document.createElement('a');
-homeLink.href = 'https://www.google.com';
-homeLink.textContent = 'Home';
-
-const aboutLink = document.createElement('a');
-aboutLink.href = 'https://about.google';
-aboutLink.textContent = 'About';
-
-const contactLink = document.createElement('a');
-contactLink.href = 'https://contacts.google.com';
-contactLink.textContent = 'Contact';
-
-// Add the links to the navigation bar
-navBar.appendChild(homeLink);
-navBar.appendChild(aboutLink);
-navBar.appendChild(contactLink);
-
-// Add the navigation bar to the HTML file
-const navContainer = document.querySelector('.nav-container');
-navContainer.appendChild(navBar);
+document.querySelector('.dropdown-content').addEventListener('mouseleave', function () {
+  this.classList.remove('expanded');
+});
